22894d0be832f5a2b920690457ca883fd1bf2267
Timofei Novik <teamix15@gmail.com>
2024-05-06 18:31:08 +0300

    add dto

---
+++ src/models/dto/todo.dto.ts
@@ -0,0 +1,6 @@
+export class TodoDTO {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+}
---
+++ src/models/dto/user.dto.ts
@@ -0,0 +1,16 @@
+export class UserDTO {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: number;
+      lng: number;
+    };
+  };
+}
